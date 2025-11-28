879ce5b055932e75319b96597232ea80cdaa9e49
Omar Eltayeb <omar.eltayeb54@gmail.com>
2025-11-28 11:12:16 +0200

    create EagleEye module

--- test_chamber/source/cheat.h
+++ test_chamber/source/cheat.h
@@ -1,6 +1,23 @@
 #ifndef CHEAT_H
 #define CHEAT_H
 
-bool is_anticheat_running();
+namespace EagleEye
+{
+    struct Connection
+    {
+#if defined(_WIN32)
+        typedef void* HANDLE;
+        HANDLE pipe;
+#endif
+
+        Connection();
+        bool is_empty() const;
+    };
+
+    Connection create_connection();
+    void send_token_request(const Connection& connection);
+
+    bool is_anticheat_running();
+}
 
 #endif
