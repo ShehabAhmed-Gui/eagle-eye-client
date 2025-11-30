a24acf85ae5cddcb52123126257868c40bbb037f
Omar Eltayeb <omar.eltayeb54@gmail.com>
2025-11-30 20:47:58 +0200

    imgui context

--- log_viewer/source/main.cpp
+++ log_viewer/source/main.cpp
@@ -2,6 +2,10 @@
 
 #include <SDL3/SDL.h>
 
+#include "../vendor/imgui/imgui.h"
+#include "../vendor/imgui/imgui_impl_sdl3.h"
+#include "../vendor/imgui/imgui_impl_sdlrenderer3.h"
+
 int main(void)
 {
 
@@ -31,6 +35,27 @@ int main(void)
     SDL_SetWindowPosition(window, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED);
     SDL_ShowWindow(window);
 
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+    //ImGui::StyleColorsLight();
+
+    // Setup scaling
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+    style.FontScaleDpi = main_scale;        // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplSDL3_InitForSDLRenderer(window, renderer);
+    ImGui_ImplSDLRenderer3_Init(renderer);
+
     bool running = true;
     while (running)
     {
@@ -45,7 +70,13 @@ int main(void)
                 running = true;
             }
         }
+
+        
     }
+
+    ImGui_ImplSDLRenderer3_Shutdown();
+    ImGui_ImplSDL3_Shutdown();
+    ImGui::DestroyContext();
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
